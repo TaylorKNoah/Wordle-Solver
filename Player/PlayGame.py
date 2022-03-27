8a514a7519aee81e35286a446db990d4f69c4b64
Taylor Noah <tnoah@webmd.net>
2022-03-27 11:27:55 -0700

    testing out seleniumbase

--- Player/PlayGame.py
+++ Player/PlayGame.py
@@ -1,26 +1,9 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from seleniumbase import BaseCase
 
-#helper function to get children of shadowdoms
-def expand_shadow_element(element):
-  shadow_root = driver.execute_script('return arguments[0].shadowRoot', element)
-  return shadow_root
-
-# open chrome
-driver = webdriver.Chrome("Player/drivers/chromedriver.exe")
-
-# nav to wordle
-print('Accessing Wordle via Chrome')
-wordle_url = "https://www.nytimes.com/games/wordle/index.html"
-driver.get(wordle_url)
-
-# bust past the shadowdoms
-print("Busting pass the shadowdoms...")
-
-game_app = driver.find_element_by_xpath("//game-app")
-shadow_root_game_app = expand_shadow_element(game_app)
-
-
-
-
-driver.close()
-print("Finished!")
+class PlayWordle(BaseCase):
+    def test(self):
+        self.open("https://www.nytimes.com/games/wordle/index.html")
+        self.click("game-app::shadow game-modal::shadow game-icon")
+        keyboard_base = "game-app::shadow game-keyboard::shadow "
